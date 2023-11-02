37cdf4b69e0489e909c23e3648a9d4cf0b371518
unknown <sandeep.kpindi@nucleusteq.com>
2023-11-02 18:51:38 +0530

    added

--- vpv.tf
+++ vpv.tf
@@ -15,7 +15,7 @@ provider "aws" {}
 provider "databricks" {
   host  = "https://dbc-a08a98d4-658e.cloud.databricks.com/.cloud.databricks.com"
 
-  token = "dapie73b2f894b4867fef3713a4647b9bf20"
+  token = "dapie8ebcd3fe10b28cc825ae00b54c1f625"
  
 }
 
